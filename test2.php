49b07ffaf851779aacc9c6609440f489a9e0d763
ZamenWolk <martin.canivez@ig2i.fr>
2015-09-23 22:46:25 +0200

    vwala v5

--- test2.php
+++ test2.php
@@ -3,5 +3,4 @@
 include ("includes/functions/passTransition.php");
 include('includes/autoload.php');
 
-$query = $db->prepare("UPDATE users SET user_password =" . encode("12345") . " WHERE user_id = 16");
-$query->execute();
+echo encode('12345');
